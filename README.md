67d395db54db3c027dacf46c915ce8c829c82760
Bruno Agretti <62401276+brunoagretti@users.noreply.github.com>
2022-04-13 16:29:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BusquedaPvZ
+Repositorio destinado a contener la solución del 1° Trabajo Práctico de la cátedra Inteligencia Artificial del 2022 
---
+++ src/main/java/busquedapvz/ChomperAgent.java
@@ -0,0 +1,21 @@
+package busquedapvz;
+
+import frsf.cidisi.faia.agent.Action;
+import frsf.cidisi.faia.agent.Perception;
+import frsf.cidisi.faia.agent.search.SearchBasedAgent;
+
+public class ChomperAgent extends SearchBasedAgent {
+
+  @Override
+  public void see(Perception p) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public Action selectAction() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
