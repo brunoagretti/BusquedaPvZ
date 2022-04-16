a88d78a4979ec2c18520e469734dc8d515e3691a
JuanWigg <jgwiggenhauser@gmail.com>
2022-04-16 20:11:30 -0300

    minor change

--- src/main/java/busquedapvz/UnknownCell.java
+++ src/main/java/busquedapvz/UnknownCell.java
@@ -1,5 +1,14 @@
 package busquedapvz;
 
 public class UnknownCell extends CellContent{
-
+	
+	@Override
+	public String toString() {
+		StringBuffer str = new StringBuffer();
+		
+		str.append("??");
+		
+		
+		return str.toString();
+	}
 }
